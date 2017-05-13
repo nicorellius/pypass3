5e538dadd5f10d445967c4c0b0c8f2b7b04884dd
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-05-13 14:43:53 -0700

    Removed SQLite database from application.

--- passgen/application.py
+++ passgen/application.py
@@ -11,9 +11,7 @@ Copyright (c) 2017 Nick Vincent-Maloney <nicorellius@gmail.com>
 
 """
 
-import sqlite3
-
-from flask import (Flask, request, session, g, redirect,
+from flask import (Flask, request, session, redirect,
                    url_for, abort, render_template, flash)
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -26,7 +24,6 @@ app.config.from_object(__name__)
 
 # Load default config and override config from an environment variable
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'pypass.db'),
     SECRET_KEY='xN~@en@B%l0Kli6TBVUoxOP(tIJ_JnC@=9(a8N8cg27J)*nQ!c',
     USERNAME='admin',
     PASSWORD='default',
@@ -39,63 +36,14 @@ app.debug = DEBUG
 toolbar = DebugToolbarExtension(app)
 
 
-def connect_db():
-    """Connects to the specific database."""
-
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-
-    return rv
-
-
-def init_db():
-
-    with app.app_context():
-
-        db = get_db()
-
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-
-        db.commit()
-
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-
-    init_db()
-
-    print('Initialized the database.')
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-
 @app.route('/')
 def show_entries():
 
-    db = get_db()
-    cur = db.execute('select type, secret from entries order by id desc')
-    entries = cur.fetchall()
+    # db = get_db()
+    # cur = db.execute('select type, secret from entries order by id desc')
+    # entries = cur.fetchall()
 
-    return render_template('generate.html', entries=entries)
+    return render_template('generate.html')  # , entries=entries)
 
 
 @app.route('/generate', methods=['POST'])
@@ -176,5 +124,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    init_db()
     app.run()
