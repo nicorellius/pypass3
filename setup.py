60d6595f2ab0f93ad1030cb709724ab1fa82e1d5
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-05-25 05:53:00 -0700

    Update setup.py with more details.

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pypass',
-    packages=['pypass'],
+    version='0.1',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'click',
+        'rdoclient',
+        'pytest',
     ],
+    entry_points="""
+        [console_scripts]
+        generate=scripts.generate:cli
+    """,
+
+    # Metadata for upload to PyPI
+    author='Nick Vincent-Maloney (nicorellius)',
+    author_email='nick@cistech.io',
+    description="""PyPass3: Password Generator application.
+        Dice Roll Optional, Mostly-Random Word, Number, 
+        and Mixed Character Password Generator""",
+    license='MIT',
+    keywords='password passphrase numbers words mixed eff dice roll',
+    url='http://pypass.cistech.io/',
 )
