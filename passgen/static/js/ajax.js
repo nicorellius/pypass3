3ad2f641c22d8bdeaad5ddb58fea90cf7ee8319c
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-05-25 21:36:15 -0700

    Updated ajax script with proper test data.

--- passgen/static/js/ajax.js
+++ passgen/static/js/ajax.js
@@ -1,7 +1,34 @@
-$.ajax({
-  type: "POST",
-  url: '/generate/',
-  data: 'test data',
-  success: 'success',
-  dataType: text
+
+// attempt to explore AJAX post
+//
+// $.ajax({
+//     type: "POST",
+//     url: '/generate',
+//     data: { output_type: "mixed" },
+//     success: 'success'
+// });
+
+$(document).ready(function() {
+
+    $('#run-again').click(function(e) {
+        e.preventDefault();
+
+        var data = {
+            number_roll: output_type,
+            number_dice: '5',
+            how_many: '1',
+            output_type: 'mixed',
+            password_length: '10'
+        };
+
+        $.ajax({
+            type: 'post',
+            url: '/generate',
+            data: data,
+            success: 'success',
+            error: 'error'
+        }).done(function() {
+            console.log('sent');
+        });
+    });
 });
