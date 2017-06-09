fa683025175ff7d8d2c11fef59a3ae84d6e636ca
Nick Vincent-Maloney <nicorellius@gmail.com>
2017-06-08 23:13:12 -0700

    Implement Oauth, Rauth, for Facebook, GitHub providers.

--- passgen/oauth.py
+++ passgen/oauth.py
@@ -1,33 +1,19 @@
-from flask import url_for
-from rauth import OAuth2Service
+import json
 
-from pypass import app as current_app
+from flask import current_app, url_for, request, redirect, session
 
-####################################
-from oauth import OAuthSignIn
-app.config['OAUTH_CREDENTIALS'] = {
-    'github': {
-        'id': 'd3be9a39c8db65911ce0',
-        'secret': 'cba32867fc777bd1291425e3aeedb222f51ef7c0'
-    },
-}
+from rauth import OAuth1Service, OAuth2Service
 
-@app.route('/authorize/github')
-def oauth_authorize(provider):
-
-    if not current_user.is_anonymous():
-        return redirect(url_for('index'))
-
-    oauth = OAuthSignIn.get_provider(provider)
-
-    return oauth.authorize()
-#########################################
+import config
+import utils
 
 
 class OAuthSignIn(object):
+
     providers = None
 
     def __init__(self, provider_name):
+
         self.provider_name = provider_name
         credentials = current_app.config['OAUTH_CREDENTIALS'][provider_name]
         self.consumer_id = credentials['id']
@@ -45,22 +31,179 @@ class OAuthSignIn(object):
 
     @classmethod
     def get_provider(cls, provider_name):
+
         if cls.providers is None:
             cls.providers = {}
+
             for provider_class in cls.__subclasses__():
                 provider = provider_class()
                 cls.providers[provider.provider_name] = provider
+
         return cls.providers[provider_name]
 
 
 class GitHubSignIn(OAuthSignIn):
+
     def __init__(self):
-        super(GitHubSignIn, self).__init__('facebook')
+
+        super(GitHubSignIn, self).__init__('github')
+
         self.service = OAuth2Service(
             name='github',
+            client_id=self.consumer_id,
+            client_secret=self.consumer_secret,
+            authorize_url='https://github.com/login/oauth/authorize',
+            access_token_url='https://github.com/login/oauth/access_token',
+            base_url='https://api.github.com'
+        )
+
+    def authorize(self):
+
+        return redirect(self.service.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_uri=self.get_callback_url())
+        )
+
+    def callback(self):
+
+        # def decode_json(payload):
+        #     config.logger.info('[{0}] Decoded payload: {1}'.format(
+        #         utils.get_timestamp(), payload.decode()))
+        #     return json.loads(payload.decode())
+
+        if 'code' not in request.args:
+            return None, None, None
+
+        oauth_session = self.service.get_auth_session(
+            data={
+                'code': request.args['code'],
+                'grant_type': 'authorization_code',
+                'redirect_uri': self.get_callback_url()
+            },
+            # decoder=decode_json
+        )
+
+        config.logger.info('[{0}] OAuth session: {1}'.format(
+            utils.get_timestamp(), oauth_session))
+
+        # social_id = 'github${0}'.format('social_id')
+        # username = 'username'
+        # email = 'email'
+
+        # the "me" response
+        me = oauth_session.get('user').json()
+        username = me['login']
+        social_id = 'github${0}'.format(me['id'])
+        email = me['url']
+        # nickname = me['name'].split()[0].lower()
+
+        return social_id, username, email  # nickname,
+
+
+class FacebookSignIn(OAuthSignIn):
+
+    def __init__(self):
+
+        super(FacebookSignIn, self).__init__('facebook')
+
+        self.service = OAuth2Service(
+            name='facebook',
             client_id=self.consumer_id,
             client_secret=self.consumer_secret,
             authorize_url='https://graph.facebook.com/oauth/authorize',
             access_token_url='https://graph.facebook.com/oauth/access_token',
             base_url='https://graph.facebook.com/'
         )
+
+    def authorize(self):
+
+        return redirect(self.service.get_authorize_url(
+            scope='email',
+            response_type='code',
+            redirect_uri=self.get_callback_url())
+        )
+
+    def callback(self):
+
+        def decode_json(payload):
+            return json.loads(payload.decode('utf-8'))
+
+        if 'code' not in request.args:
+            return None, None, None
+
+        oauth_session = self.service.get_auth_session(
+            data={
+                'code': request.args['code'],
+                'grant_type': 'authorization_code',
+                'redirect_uri': self.get_callback_url()
+            },
+            decoder=decode_json
+        )
+
+        me = oauth_session.get('me?fields=id,email').json()
+        config.logger.info('[{0}] Me in provider callback: {1}'.format(
+            utils.get_timestamp(), me))
+
+        # Facebook does not provide a username, so the user email is used
+        # to extract one out
+
+        social_id = 'facebook${0}'.format(me['id'])
+        username = me.get('email').split('@')[0]
+        email = me.get('email')
+
+        return social_id, username, email
+
+
+class TwitterSignIn(OAuthSignIn):
+
+    def __init__(self):
+
+        super(TwitterSignIn, self).__init__('twitter')
+
+        self.service = OAuth1Service(
+            name='twitter',
+            consumer_key=self.consumer_id,
+            consumer_secret=self.consumer_secret,
+            request_token_url='https://api.twitter.com/oauth/request_token',
+            authorize_url='https://api.twitter.com/oauth/authorize',
+            access_token_url='https://api.twitter.com/oauth/access_token',
+            base_url='https://api.twitter.com/1.1/'
+        )
+
+    def authorize(self):
+
+        request_token = self.service.get_request_token(
+            params={
+                'oauth_callback': self.get_callback_url()
+            }
+        )
+
+        session['request_token'] = request_token
+
+        return redirect(self.service.get_authorize_url(request_token[0]))
+
+    def callback(self):
+
+        def decode_json(payload):
+            return json.loads(payload.decode('utf-8'))
+
+        request_token = session.pop('request_token')
+
+        if 'oauth_verifier' not in request.args:
+            return None, None, None
+
+        oauth_session = self.service.get_auth_session(
+            request_token[0],
+            request_token[1],
+            data={
+                'oauth_verifier': request.args['oauth_verifier']
+            },
+            decoder=decode_json
+        )
+
+        me = oauth_session.get('account/verify_credentials.json').json()
+        social_id = 'twitter${0}'.format(str(me.get('id')))
+        username = me.get('screen_name')
+
+        return social_id, username, None  # Twitter does not provide email
--- passgen/pypass.py
+++ passgen/pypass.py
@@ -11,11 +11,17 @@ Copyright (c) 2017 Nick Vincent-Maloney <nicorellius@gmail.com>
 
 """
 import logging
+from sqlalchemy.exc import IntegrityError
 
 from flask import (Flask, request, session, redirect,
                    url_for, abort, render_template, flash)
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import (LoginManager, UserMixin,
+                         login_user, logout_user, current_user)
+
 # from flask_pymongo import PyMongo
+from oauth import OAuthSignIn
 
 import utils
 import config
@@ -34,9 +40,25 @@ app.debug = config.DEBUG
 # Load default config and override config from an environment variable
 app.config.update(dict(
     SECRET_KEY='xN~@en@B%l0Kli6TBVUoxOP(tIJ_JnC@=9(a8N8cg27J)*nQ!c',
-    USERNAME='admin',
-    PASSWORD='default',
+    USERNAME='guest',
+    PASSWORD='password',
     DEBUG_TB_INTERCEPT_REDIRECTS=False,
+    SQLALCHEMY_DATABASE_URI='sqlite:///pypass.db',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    OAUTH_CREDENTIALS={
+        'github': {
+            'id': 'd3be9a39c8db65911ce0',
+            'secret': 'cba32867fc777bd1291425e3aeedb222f51ef7c0'
+        },
+        'facebook': {
+            'id': '137292646828195',
+            'secret': 'e9ba084118895fc5dc82ed69eb6a3330'
+        },
+        'twitter': {
+            'id': ' g3wINYT3Y3jI5iuxCFEJ5meG2',
+            'secret': 'MH7c6kHwi7ICvyE0PyJl0Ezf7xJG7z15StmznMBG3TTcdE943p'
+        }
+    }
 ))
 
 if app.debug is True:
@@ -56,15 +78,29 @@ csrf(app)
 #
 # mongo = PyMongo(app)
 
-# Set up logging configuration and get logger
-# TODO: set up proper logging app with handler, formatter, etc...
-logging.basicConfig(
-    # filename='output.log',
-    format='%(levelname)s %(message)s',
-    level=logging.DEBUG
-)
+db = SQLAlchemy(app)
+lm = LoginManager(app)
+lm.login_view = 'home'
 
-logger = logging.getLogger(__name__)
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), nullable=False, unique=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=True)
+
+    def __init__(self, username, social_id, nickname, email=None):
+        self.username = username
+        self.social_id = social_id
+        self.email = email
+        self.nickname = nickname
+
+    def __repr__(self):
+        return '{0}'.format(self.username)
 
 
 # Start views for main application
@@ -149,11 +185,11 @@ def generate():
 
             elif length == 32:
                 secret = utils.gen_uid()
-                flash(secret)
+                flash(secret, 'secrets')
 
             else:
                 secret = utils.gen_uid(length, True)
-                flash(secret)
+                flash(secret, 'secrets')
 
             return redirect(url_for('home'))
 
@@ -169,14 +205,14 @@ def generate():
             #     'K1vGZPsxgd6SEmkpD?xIG-g_8-GnIC!8)EPzk_=45chrNE%51g')
             # )
 
-            flash(secret)
+            flash(secret, 'secrets')
 
             _log_output_params(output_type, dice, rolls, length, num)
 
             return redirect(url_for('home'))
 
         except Exception as e:
-            print("Exception: {0}".format(e))
+            print(("Exception: {0}".format(e)))
 
 
 # @app.route('/settings')
@@ -201,34 +237,48 @@ def login():
 
     if request.method == 'POST':
 
-        if request.form['username'] != app.config['USERNAME']:
-            error = ' Invalid username or password'
+        username = request.form['username']
+        password = request.form['password']
 
-        elif request.form['password'] != app.config['PASSWORD']:
+        config_user = app.config['USERNAME']
+        config_pass = app.config['PASSWORD']
+
+        if username != config_user or password != config_pass:
             error = ' Invalid username or password'
 
         else:
+            # try:
+            #     default = User(username, username, username)
+            #     db.session.add(default)
+            #     db.session.commit()
+            # except Exception as e:
+            #     print(e)
 
             session['logged_in'] = True
+            flash('You were logged in', 'notifications')
 
-            flash('You were logged in')
             return redirect(url_for('home'))
 
-    return render_template('login.html', error=error)
+    data = {
+        'error': error,
+    }
+
+    return render_template('login.html', **data)
 
 
 @app.route('/logout', methods=['GET'])
 def logout():
 
     session.pop('logged_in', None)
-    flash('You were logged out')
+    logout_user()
+    flash('You were logged out', 'notifications')
 
     return redirect(url_for('login'))
 
 
 def _log_output_params(output_type, dice, rolls, length, num):
 
-    return logger.info(
+    return config.logger.info(
         '[{0}] Parameters:\n'
         '        output type: {1} {2}\n'
         '     number of dice: {3}     {4}\n'
@@ -244,5 +294,75 @@ def _log_output_params(output_type, dice, rolls, length, num):
     )
 
 
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.route('/authorize/<provider>')
+def oauth_authorize(provider):
+
+    config.logger.info('[{0}] Before get_provider: {1}'.format(
+        utils.get_timestamp(), str(provider)))
+
+    if not current_user.is_anonymous:
+        return redirect(url_for('home'))
+
+    oauth = OAuthSignIn.get_provider(provider)
+
+    config.logger.info('[{0}] After get_provider: {1}'.format(
+        utils.get_timestamp(), str(provider)))
+
+    config.logger.info('[{0}] OAuth authorize: {1}'.format(
+        utils.get_timestamp(), oauth.authorize()))
+
+    return oauth.authorize()
+
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+
+    if not current_user.is_anonymous:
+        return redirect(url_for('home'))
+
+    oauth = OAuthSignIn.get_provider(provider)
+    social_id, username, email = oauth.callback()
+
+    config.logger.info('[{0}] OAuth: {1}'.format(
+        utils.get_timestamp(), oauth))
+
+    if social_id is None:
+        flash('Authentication failed.')
+
+        return redirect(url_for('home'))
+
+    user = User.query.filter_by(social_id=social_id).first()
+    config.logger.info('[{0}] Me in oauth_callback: {1}'.format(
+        utils.get_timestamp(), user))
+
+    if not user:
+
+        try:
+            user = User(username=username, social_id=social_id,
+                        nickname='temp', email=email)
+            db.session.add(user)
+            db.session.commit()
+
+        except IntegrityError:
+            login_user(user, True)
+            session['logged_in'] = True
+            flash("Only one '{0}' can access this system.\n"
+                  "Logged in as 'guest' instead.".format(username),
+                  'notifications')
+            return redirect(url_for('home'))
+
+    login_user(user, True)
+    session['logged_in'] = True
+    flash('You were logged in', 'notifications')
+
+    return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run()
